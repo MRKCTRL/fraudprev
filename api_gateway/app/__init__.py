39f76ac85ee425c5af0890f3bf7f256ad6e5a08b
MRKCTRL <davidmbundu@gmail.com>
2024-09-13 17:06:58 +0200

    gateway services

--- api_gateway/app/__init__.py
+++ api_gateway/app/__init__.py
@@ -2,6 +2,8 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS 
 import os 
+import requests
+from flask import Flask, jsonify
 
 
 db=SQLAlchemy()
@@ -10,6 +12,20 @@ db=SQLAlchemy()
 
 def create():
     app=Flask(__name__)
+    
+    @app.route('/<service>/<path:path>', methods=['GET','POST','PUT'])
+    def proxy(service, path):
+        if service=='auth':
+            target_url=f"http://auth_service:5000/{path}"
+        elif service=='payment':
+            target_url=f"http://payment_service:5001/{path}"
+        elif service =='fraud':
+            target_url=f"http://fraud_service:50002/{path}"
+        else: 
+            return jsonify({'error': 'Service not found'}), 404
+        
+        response= requests.requests(method=requests.method, url=target_url, json=requests.get_json())
+        return jsonify(response.json()), response.status_code
     app.config['SECRET_KEY']=os.getenv('SECRET_KEY')
     app.config['SQLALCHEMY_DATABASE_URI']=os.getenv('DATABASE_URL')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
